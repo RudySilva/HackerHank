1d5ba8bd6ce5e11fd1b41c863b21e355b7baaff7
Rudy <rudysilva22@gmail.com>
2019-09-22 15:54:31 +1000

    Day 14 - Scope

---
+++ HackerHank/Challenges/Scope.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerHank
+{
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference(int[] a)
+        {
+            this.elements = a;
+        }
+
+        public int computeDifference()
+        {
+            maximumDifference = 0;
+            int difference;
+            int highest;
+            int lowest;
+            for (int i = 0; i < elements.Length; i++)
+            {
+                difference = 0;
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        if (elements[i] > elements[j])
+                        {
+                            highest = elements[i];
+                            lowest = elements[j];
+                        }
+                        else
+                        {
+                            highest = elements[j];
+                            lowest = elements[i];
+                        }
+
+                        difference = highest - lowest;
+
+                        if (maximumDifference < difference)
+                            maximumDifference = difference;
+                    }
+
+                }
+            }
+            return maximumDifference;
+        }
+
+    } 
+
+    class Scope
+    {
+        public static void Execute()
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+}
